c710f089f6e6799c28889cb1a854e84d06844cee
i.segodin <igor.segodin@gmail.com>
2020-05-18 19:34:16 +0300

    Digit recognition

--- src/main/java/org/isegodin/ml/customnetwork/App.java
+++ src/main/java/org/isegodin/ml/customnetwork/App.java
@@ -6,17 +6,16 @@ import org.isegodin.ml.customnetwork.network.SimpleNeuralNetwork;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.image.Raster;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author isegodin
@@ -50,64 +49,106 @@ public class App {
                         .addLayer(15)
         );
 
+        final AtomicLong alpha = new AtomicLong(Double.doubleToLongBits(0.5));
 
         final int chunkSize = 100;
-        final AtomicInteger counter = new AtomicInteger();
+        final AtomicInteger batchCounter = new AtomicInteger();
         final List<SimpleNeuralNetwork.TrainData> trainDataList = new ArrayList<>(chunkSize);
 
-        Files.list(Paths.get(MNIST_FOLDER, "train"))
-                .forEach(p -> {
-                    if (!trainDataList.isEmpty() && counter.getAndIncrement() % chunkSize == 0) {
-                        // TODO train
-                        trainDataList.clear();
-                    }
+        double avgError = 1;
+        boolean first = true;
 
-                    trainDataList.add(
-                            extractTranDataFromImage(p)
-                    );
-                });
+        while (avgError > 0.3) {
+
+            trainDataList.clear();
+            batchCounter.set(0);
+
+            Files.list(Paths.get(MNIST_FOLDER, "train"))
+                    .forEach(p -> {
+                        if (batchCounter.getAndIncrement() % chunkSize == 0 && !trainDataList.isEmpty()) {
+
+                            neuralNetwork.train(trainDataList, Double.longBitsToDouble(alpha.get()));
+
+                            trainDataList.clear();
+                        }
+
+                        trainDataList.add(
+                                extractTranDataFromImage(p)
+                        );
+                    });
+
+            neuralNetwork.addEpoch();
+
+            neuralNetwork.save(MODEL_FOLDER);
 
 
-//        neuralNetwork.addEpoch();
-//
-//        neuralNetwork.save(MODEL_FOLDER);
+            trainDataList.clear();
+            batchCounter.set(0);
+
+            AtomicLong totalError = new AtomicLong();
+            AtomicInteger count = new AtomicInteger();
 
 
-//        BufferedImage read = ImageIO.read(new File("/Users/isegodin/GitHub/machine-learning-custom-network/mnist/train/000000-num5.png"));
-//        int[] pixel = read.getData().getPixel(13, 13, new int[1]);
+            Files.list(Paths.get(MNIST_FOLDER, "test"))
+                    .forEach(p -> {
+                        if (batchCounter.getAndIncrement() % chunkSize == 0 && !trainDataList.isEmpty()) {
 
-        System.out.println();
+                            double error = neuralNetwork.evaluate(trainDataList);
 
+                            count.getAndIncrement();
 
-//        NeuralNetworkData neuralNetworkData;
-//
-//        try (InputStream is = new FileInputStream(new File("/Users/isegodin/GitHub/machine-learning-custom-network/nn-data.json"))) {
-//            neuralNetworkData = objectMapper.readValue(is, NeuralNetworkData.class);
-//        }
-//
-//        double[] input = {0.05, 0.1};
-//
-//        FeedforwardResultData calcResult = NeuralNetworkResultCalculator.calcResult(input, neuralNetworkData);
-//
-//        double[] expectedOut = {0.7513650695523157, 0.7729284653214625};
-//
-//        System.out.println(neuralNetworkData);
-//        System.out.println(calcResult);
-//        System.out.println("Expected: " + Arrays.toString(expectedOut));
-//        System.out.println("Actual:   " + Arrays.toString(calcResult.getFinalOut()));
-//
-//
-//        NeuralNetworkBackpropagationAlgorithm.train(new double[]{0.01, 0.99}, calcResult, neuralNetworkData, 0.5);
+                            totalError.set(
+                                    Double.doubleToLongBits(error + Double.longBitsToDouble(totalError.get()))
+                            );
 
+                            trainDataList.clear();
+                        }
+
+                        trainDataList.add(
+                                extractTranDataFromImage(p)
+                        );
+                    });
+
+            double newError = Double.longBitsToDouble(totalError.get()) / count.get();
+
+            if (!first) {
+
+                double newAlpha = Double.longBitsToDouble(alpha.get());
+                newAlpha += newAlpha * (avgError - newError) / avgError * -1;
+                newAlpha = Math.min(0.5, newAlpha);
+
+                alpha.set(Double.doubleToLongBits(newAlpha));
+            }
+
+            avgError = newError;
+
+            first = false;
+
+            System.out.println("Test avgError = " + avgError + ", alpha = " + Double.longBitsToDouble(alpha.get()));
+        }
     }
 
-    private static SimpleNeuralNetwork.TrainData extractTranDataFromImage(Path image) {
-        String filename = image.getFileName().toString();
+    @SneakyThrows
+    private static SimpleNeuralNetwork.TrainData extractTranDataFromImage(Path imagePath) {
+        String filename = imagePath.getFileName().toString();
         String filenameWithoutExtension = filename.substring(0, filename.lastIndexOf("."));
         int number = Integer.valueOf(
                 filenameWithoutExtension.split("-")[1].substring("num".length())
         );
 
-        return new SimpleNeuralNetwork.TrainData();
+        BufferedImage bufferedImage = ImageIO.read(imagePath.toFile());
+        Raster imageData = bufferedImage.getData();
+
+        double[] input = new double[bufferedImage.getHeight() * bufferedImage.getWidth()];
+
+        for (int y = 0; y < bufferedImage.getHeight(); y++) {
+            for (int x = 0; x < bufferedImage.getWidth(); x++) {
+                int idx = x + y * bufferedImage.getWidth();
+
+                input[idx] = imageData.getPixel(x, y, new int[1])[0];
+            }
+        }
+
+        return new SimpleNeuralNetwork.TrainData(input, digitTargetMap.get(number));
     }
 }
